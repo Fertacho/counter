3313ce307d8c8234d56434d05f8aa21a4c0b76f8
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-05-26 20:22:36 +0000

    avance

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,21 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function counter(props) {
+	return (
+		<div className="SecCounter">
+			<diV className="clock">
+				<i class="fa-light fa-clock"></i>
+			</diV>
+			<div className="Cmil"></div>
+			<div className="Dmil"></div>
+			<div className="Umil"></div>
+			<div className="centena"></div>
+			<div className="decena"></div>
+			<div className="Unidad"></div>
+		</div>
+	);
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<counter />, document.querySelector("#app"));
